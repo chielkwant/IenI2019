5b26f0fb2903f622c88827b078f83027a199dc10
chielkwant <151102@csgaugustinus.nl>
2019-11-20 10:18:50 +0000

    hdydthhd

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,6 +1,6 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
@@ -9,4 +9,10 @@ function draw() {
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+    noStroke();
+    fill("pink");
+    triangle(450,0,450,450,0,225);
+    noStroke();
+    fill("purple");
+    triangle(0,450,0,0,450,225);
 }
